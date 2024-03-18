cb6dc9a44ad53ebe504fe9a0e8fe906ad7eb0c07
Alex Norment <87099499+GiltedShadow@users.noreply.github.com>
2024-03-18 15:29:24 -0700

    Learning one!

---
+++ LearningJava/LearningJava/src/main/java/com/mycompany/learningjava/LearningJava.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.learningjava;
+
+/**
+ *
+ * @author never
+ */
+public class LearningJava {
+
+    public static void main(String[] args) {
+        
+        int a = 5;
+        char b = 'c';
+        long c = 400;
+        double d = 3.2;
+        
+        String name = "Xavier";
+        System.out.println(name);
+        
+        
+        System.out.println("Hello World!");
+    }
+    
+    public static void addExclimationPoint(String s){
+        System.out.println(s + "!");
+    }
+}
