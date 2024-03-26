56bf8c020135dcadc4f7a9009428dc842019ea44
Alex Norment <87099499+GiltedShadow@users.noreply.github.com>
2024-03-26 15:31:07 -0700

    calculate pi start

---
+++ CalculatePi/src/main/java/com/mycompany/calculatepi/CalculatePi.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatepi;
+
+/**
+ *
+ * @author never
+ */
+public class CalculatePi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+        /*
+         Below is python code to be transferred, it runs a bit slow
+# Initialize denominator
+k = 1
+ 
+# Initialize sum
+s = 0
+ 
+for i in range(1000000):
+ 
+    # even index elements are positive
+    if i % 2 == 0:
+        s += 4/k
+    else:
+        # odd index elements are negative
+        s -= 4/k
+ 
+    # denominator is odd
+    k += 2
+     
+print(s)
+        */
+    }
+}
